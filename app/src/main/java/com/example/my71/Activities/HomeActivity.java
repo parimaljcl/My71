6ec0e9626ee88f53e24c4d726a957124a7d60d03
mohiuddintarek <41743928+mohiuddin-Tarek@users.noreply.github.com>
2019-12-12 11:34:37 +0600

    Added home activity and changed luncher to home activity

---
+++ app/src/main/java/com/example/my71/Activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.my71.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.my71.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
